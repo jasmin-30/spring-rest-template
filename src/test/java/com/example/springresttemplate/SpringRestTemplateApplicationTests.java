67b6a2e6e73de2688c7add15227ca5a4753bb8c1
Jasmin Makwana <jasminmakwana1999@gmail.com>
2024-07-08 15:52:53 +0530

    Updated package name

--- src/test/java/com/example/springresttemplate/SpringRestTemplateApplicationTests.java
+++ src/test/java/com/example/springresttemplate/SpringRestTemplateApplicationTests.java
@@ -1,4 +1,4 @@
-package com.example.spring_rest_template;
+package com.example.springresttemplate;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
