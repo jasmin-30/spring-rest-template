d37891a4db9111e8532f5c65ce819b50bd247eed
Jasmin Makwana <jasminmakwana1999@gmail.com>
2024-07-08 16:29:08 +0530

    Implemented Spring Pageable with Jackson

--- src/main/java/com/example/springresttemplate/client/BeerClientImpl.java
+++ src/main/java/com/example/springresttemplate/client/BeerClientImpl.java
@@ -1,15 +1,12 @@
 package com.example.springresttemplate.client;
 
 import com.example.springresttemplate.model.BeerDTO;
-import com.fasterxml.jackson.databind.JsonNode;
 import lombok.RequiredArgsConstructor;
 import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.data.domain.Page;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
-
-import java.util.Map;
 
 @RequiredArgsConstructor
 @Service
@@ -26,18 +23,20 @@ public class BeerClientImpl implements BeerClient {
     public Page<BeerDTO> listAllBeers() {
         RestTemplate restTemplate = restTemplateBuilder.build();
 
-        ResponseEntity<String>  stringResponse = restTemplate.getForEntity(BASE_PATH + GET_BEER_PATH, String.class);
-        System.out.println(stringResponse.getBody());
+//        ResponseEntity<String>  stringResponse = restTemplate.getForEntity(BASE_PATH + GET_BEER_PATH, String.class);
+//        System.out.println(stringResponse.getBody());
+//        ResponseEntity<Map>  mapResponse = restTemplate.getForEntity(BASE_PATH + GET_BEER_PATH, Map.class);
+//        System.out.println(mapResponse.getBody());
+//
+//        ResponseEntity<JsonNode>  jsonResponse = restTemplate.getForEntity(BASE_PATH + GET_BEER_PATH, JsonNode.class);
+//
+//        jsonResponse.getBody().findPath("content")
+//                .elements().forEachRemaining(jsonNode -> {
+//                    System.out.println(jsonNode.get("beerName").asText());
+//                });
 
-        ResponseEntity<Map>  mapResponse = restTemplate.getForEntity(BASE_PATH + GET_BEER_PATH, Map.class);
-        System.out.println(mapResponse.getBody());
-
-        ResponseEntity<JsonNode>  jsonResponse = restTemplate.getForEntity(BASE_PATH + GET_BEER_PATH, JsonNode.class);
-
-        jsonResponse.getBody().findPath("content")
-                .elements().forEachRemaining(jsonNode -> {
-                    System.out.println(jsonNode.get("beerName").asText());
-                });
+        ResponseEntity<BeerDTOPageImpl> pageResponse = restTemplate.getForEntity(BASE_PATH + GET_BEER_PATH, BeerDTOPageImpl.class);
+        System.out.println(pageResponse.getBody());
 
         return null;
     }
