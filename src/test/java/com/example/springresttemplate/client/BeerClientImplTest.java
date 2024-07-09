9e1ff9dc16c977c13eba5ae4609360e09e3088ab
Jasmin Makwana <jasminmakwana1999@gmail.com>
2024-07-09 12:45:07 +0530

    Added test code for delete beer using HTTP delete method.

--- src/test/java/com/example/springresttemplate/client/BeerClientImplTest.java
+++ src/test/java/com/example/springresttemplate/client/BeerClientImplTest.java
@@ -5,17 +5,33 @@ import com.example.springresttemplate.model.BeerStyle;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.web.client.HttpClientErrorException;
 
 import java.math.BigDecimal;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
 class BeerClientImplTest {
 
     @Autowired
     BeerClient beerClient;
+
+    @Test
+    void testDeleteBeer() {
+        BeerDTO dto = BeerDTO.builder()
+                .beerName("Mango Bobs")
+                .price(new BigDecimal("10.99"))
+                .beerStyle(BeerStyle.IPA)
+                .quantityOnHand(500)
+                .upc("123245")
+                .build();
+
+        BeerDTO newDto = beerClient.createBeer(dto);
+
+        beerClient.deleteBeer(newDto.getId());
+        assertThrows(HttpClientErrorException.class, () -> beerClient.getBeerById(newDto.getId()));
+    }
 
     @Test
     void testUpdateBeer() {
