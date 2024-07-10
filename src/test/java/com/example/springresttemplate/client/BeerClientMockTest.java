638e85c36ce907e978d7bcaf852aeb210dcf621d
Jasmin Makwana <jasminmakwana1999@gmail.com>
2024-07-10 12:30:17 +0530

    Added mock test case for delete beer

--- src/test/java/com/example/springresttemplate/client/BeerClientMockTest.java
+++ src/test/java/com/example/springresttemplate/client/BeerClientMockTest.java
@@ -120,6 +120,16 @@ class BeerClientMockTest {
         assertThat(returnedDto.getId()).isEqualTo(dto.getId());
     }
 
+    @Test
+    void testDeleteBeer() {
+        server.expect(method(HttpMethod.DELETE))
+                .andExpect(requestToUriTemplate(URL + BeerClientImpl.GET_BEER_BY_ID_PATH, dto.getId()))
+                .andRespond(withNoContent());
+
+        beerClient.deleteBeer(dto.getId());
+        server.verify();
+    }
+
     BeerDTO getBeerDto(){
         return BeerDTO.builder()
                 .id(UUID.randomUUID())
